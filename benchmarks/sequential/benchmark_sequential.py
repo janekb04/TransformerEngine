30d188cf650d1448782956d4280b25519487fbb8
Jan Bielak <jbielak@nvidia.com>
2025-06-10 18:31:07 +0000

    Added testing layers with FP8 enabled
    Signed-off-by: Jan Bielak <jbielak@nvidia.com>

--- benchmarks/sequential/benchmark_sequential.py
+++ benchmarks/sequential/benchmark_sequential.py
@@ -5,6 +5,7 @@
 import torch
 from torch import nn
 import transformer_engine.pytorch as te
+from transformer_engine.common.recipe import Format, DelayedScaling
 from fused_te_layer import FusedTETransformerLayer
 from sequential_te_layer import SequentialTETransformerLayer
 from utils import speedometer
@@ -45,18 +46,28 @@ builtin_te_transformer_layer.to(dtype=DTYPE).cuda()
 x = torch.rand(SEQUENCE_LENGTH, BATCH_SIZE, HIDDEN_SIZE).cuda().to(dtype=DTYPE)
 dy = torch.rand(SEQUENCE_LENGTH, BATCH_SIZE, HIDDEN_SIZE).cuda().to(dtype=DTYPE)
 
+# Setup fp8 autocast kwargs
+fp8_format = Format.HYBRID
+fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo="max")
+fp8_autocast_kwargs = { "enabled": True, "fp8_recipe": fp8_recipe }
+
 # Test layers
-def test_layer(layer: nn.Module, name: str):
+def _test_layer(layer: nn.Module, name: str, fp8_autocast_kwargs: dict | None):
     print(name)
     with nvtx.annotate(name):
         mean_ms = speedometer(
             layer,
             x,
             dy,
-            forward_kwargs = { "attention_mask": None },
+            { "attention_mask": None },
+            fp8_autocast_kwargs
         )
     print(f"Mean time: {mean_ms:.2f} ms")
 
-test_layer(fused_te_transformer_layer, "Fused TE Layer")
-test_layer(sequential_te_transformer_layer, "Sequential TE Layer")
-test_layer(builtin_te_transformer_layer, "Builtin TE TransformerLayer")
+def test_layer_with_without_fp8(layer: nn.Module, name: str):
+    _test_layer(layer, name, None)
+    _test_layer(layer, name + " (FP8)", fp8_autocast_kwargs)
+
+test_layer_with_without_fp8(fused_te_transformer_layer, "Fused TE Layer")
+test_layer_with_without_fp8(sequential_te_transformer_layer, "Sequential TE Layer")
+test_layer_with_without_fp8(builtin_te_transformer_layer, "Builtin TE TransformerLayer")
